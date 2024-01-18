6eb1be9edd413f80fbc39a88fc91d871ddd8fe5f
solchef <jawiwy@gmail.com>
2024-01-18 22:09:54 +0400

    commit -watches

--- index.js
+++ index.js
@@ -1,71 +1,140 @@
 const express = require("express");
 const fs = require("fs");
+const axios = require("axios");
 
-const { scrapeBrands, scrapeIndex, scrapeCollections } = require("./watchcharts/scraping");
+const {
+	scrapeBrands,
+	scrapeIndex,
+	scrapeCollections,
+} = require("./watchcharts/scraping");
 
 // Create an Express application
 const app = express();
 
-  
-app.get('/market-index', async (req, res) => {
-    try {
-      const jsonFileUrl = 'https://watchcharts.com/charts/brand.json'; // Replace with the actual URL
-      const data = await scrapeIndex(jsonFileUrl);
+app.get("/market-index", async (req, res) => {
+	try {
+		const jsonFileUrl = "https://watchcharts.com/charts/brand.json"; // Replace with the actual URL
+		const data = await scrapeIndex(jsonFileUrl);
 
+		const jsonOutputPath = "market_index.json";
 
-      const jsonOutputPath = "market_index.json";
+		if (data) {
+			// Read the existing JSON file only if there's no new data to write
 
-        if (data) {
-          // Read the existing JSON file only if there's no new data to write
-          
-         res.json([{ success: true, data }]);
-        } else {
-          // Write the scraped data to the JSON file if there's no existing data
-          const existingData = fs.readFileSync(jsonOutputPath, 'utf8'); // Read as text
-          res.send([JSON.parse(existingData)])
-        }
+			res.json([{ success: true, data }]);
+		} else {
+			// Write the scraped data to the JSON file if there's no existing data
+			const existingData = fs.readFileSync(jsonOutputPath, "utf8"); // Read as text
+			res.send([JSON.parse(existingData)]);
+		}
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ success: false, error: "Internal Server Error" });
+	}
+});
 
+// Route 2
+app.get("/sync/brands", async (req, res) => {
+	try {
+		const jsonFileUrl = "https://watchcharts.com/watches/brands"; // Replace with the actual URL
+		const data = await scrapeBrands(jsonFileUrl);
+		res.json({ success: true, data });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ success: false, error: error });
+	}
+});
+
+app.get("/oldbrands", async (req, res) => {
+	try {
+		const jsonFileUrl = "https://watchcharts.com/watches/brands"; // Replace with the actual URL
+		const data = await scrapeBrands(jsonFileUrl);
+		res.json({ success: true, data });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ success: false, error: error });
+	}
+});
+
+app.get("/collections", async (req, res) => {
+	try {
+		const jsonFileUrl = "https://watchcharts.com/watches/brand/breitling"; // Replace with the actual URL
+		const data = await scrapeCollections(jsonFileUrl);
+		res.json({ success: true, data });
+	} catch (error) {
+		console.log(error);
+		res.status(500).json({ success: false, error: error });
+	}
+});
+
+app.get("/get-brands", async (req, res) => {
+	const options = {
+		method: "GET",
+		url: "https://chrono241.p.rapidapi.com/brands",
+		headers: {
+			"X-RapidAPI-Key": "be31f8812bmshaf5a1e9248a26f6p1aec23jsn676eaede7a19",
+			"X-RapidAPI-Host": "chrono241.p.rapidapi.com",
+		},
+	};
+
+	try {
+		const response = await axios.request(options);
+		console.log(response);
+    res.json(response.data);
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+app.get("/get-models", async (req, res) => {
+
+  const brand = req.query.brand;
+
+  const options = {
+    method: 'GET',
+    url: 'https://chrono241.p.rapidapi.com/models',
+    params: {brandID: brand},
+    headers: {
+      'X-RapidAPI-Key': 'be31f8812bmshaf5a1e9248a26f6p1aec23jsn676eaede7a19',
+      'X-RapidAPI-Host': 'chrono241.p.rapidapi.com'
+    }
+  };
+
+	try {
+		const response = await axios.request(options);
+		console.log(response);
+    res.json(response.data);
+	} catch (error) {
+		console.error(error);
+	}
+});
+
+app.get("/get-perfomance-index", async (req, res) => {
+
+  const options = {
+    method: 'GET',
+    url: 'https://www.chrono24.co.uk/api/priceindex/performance-chart.json?type=Market&period=max',
+    headers: {
+      'X-RapidAPI-Key': 'be31f8812bmshaf5a1e9248a26f6p1aec23jsn676eaede7a19',
+      'X-RapidAPI-Host': 'chrono241.p.rapidapi.com'
+    }
+  };
+
+  try {
+		const response = await axios.request(options);
+		// console.log(response);
+    res.json(response.data);
+	} catch (error) {
+		console.error(error);
+	}
       
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ success: false, error: 'Internal Server Error' });
-    }
-  });
-  
-  // Route 2
-  app.get('/sync/brands', async (req, res) => {
-    try {
-      const jsonFileUrl = 'https://watchcharts.com/watches/brands'; // Replace with the actual URL
-      const data = await scrapeBrands(jsonFileUrl);
-      res.json({ success: true, data });
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ success: false, error: error });
-    }
-  });
-
-  app.get('/brands', async (req, res) => {
-    try {
-      const jsonFileUrl = 'https://watchcharts.com/watches/brands'; // Replace with the actual URL
-      const data = await scrapeBrands(jsonFileUrl);
-      res.json({ success: true, data });
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ success: false, error: error });
-    }
-  });
+});
 
 
-  app.get('/collections', async (req, res) => {
-    try {
-      const jsonFileUrl = 'https://watchcharts.com/watches/brand/breitling'; // Replace with the actual URL
-      const data = await scrapeCollections(jsonFileUrl);
-      res.json({ success: true, data });
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ success: false, error: error });
-    }
-  });
+
+
+
+
 
 
 const port = 3002;
